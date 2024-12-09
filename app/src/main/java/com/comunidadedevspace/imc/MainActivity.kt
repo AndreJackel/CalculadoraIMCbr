ca2ede03a8304b90f6cff4e3ad3c0383d57b3a15
Andre Jackel <andrejackel8722@gmail.com>
2024-12-09 22:50:17 +0100

    Initialize compose_refactor

---
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.comunidadedevspace.imc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContent {
+            IMCTheme {
+                AppNavigator(initialScreen = MyRoutes.CALCULATE_SCREEN)
+            }
+        }
+    }
+}
